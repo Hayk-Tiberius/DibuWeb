16770e406350251c0cd098e2d4299f2c4fc1ffa2
Hayk Marius <avrecij02@gmail.com>
2025-02-17 23:01:59 +0300

    Dibu Main Done

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -5,15 +5,25 @@ import Footer from "../tools/Footer";
 const Main = () => {
     return ( 
         <>
-            <Menu />
+        
             <main>
                 <div className="main__title">
-                    <img src={require('../img/selection/main.png')} alt="" height={'884vh'}  />
-                    <span className="title">ДМИТРИЙ БУЯНОВ</span>
+                    <span className="order__title">Заказать мероприятие</span>
+                    <img src={require('../img/selection/main.png')} alt="" height={'924vh'}  />
+                    <span className="title">ДМИТРИЙ <span >БУЯНОВ</span></span>
+                    <div className="messanger__title">
+                        <a href="https://vk.com/dibuman" onClick={(e) => { e.preventDefault(); window.open("https://vk.com/dibuman", '_blank'); }}>
+                            <img src={require("../img/icons/vk.webp")} alt="" width={"70wv"} style={{ border: "0.2em solid black", borderRadius: "1.2em", padding: "0.3em" }} />
+                        </a>
+                        <a href="https://t.me/Event_Buyanov" onClick={(e) => { e.preventDefault(); window.open("https://t.me/Event_Buyanov", '_blank'); }}>
+                            <img src={require("../img/icons/telegram.webp")} alt="" width={"70wv"} />
+                        </a>
+                        <a href="www.instagram.com/tot_samiy_dibu" onClick={(e) => { e.preventDefault(); window.open("www.instagram.com/tot_samiy_dibu", '_blank'); }}>
+                            <img src={require("../img/icons/instagramm.png")} alt="" width={"72wv"} />
+                        </a>
+                    </div>
                 </div>
-                <div>
-
-                </div>
+              
             </main>
             <Footer />
         </>
